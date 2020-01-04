87edad1d928c398447a905e74d34c31cd9a288ca
Xsitsu <jacoblocke21@live.com>
2020-01-04 22:25:43 +0900

    Completed E04

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+	public Item item;
+
+	public override void Interact()
+	{
+		base.Interact();
+
+		PickUp();
+	}
+
+	void PickUp()
+	{
+		Debug.Log("Pickup item: " + item.name);
+
+		bool didPickUp = Inventory.instance.Add(item);
+		if (didPickUp)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
