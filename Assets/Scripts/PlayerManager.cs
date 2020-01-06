939b7b1fff1115a59dca0ed2d111ae5e7980a161
Xsitsu <jacoblocke21@live.com>
2020-01-06 23:50:05 +0900

    Completed E10.

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+	public static PlayerManager instance;
+	void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("More than one instance of PlayerManager found!");
+			return;
+		}
+
+		instance = this;
+	}
+
+	public GameObject player;
+}
